33dde31c60b03cbe743c08863d79fd1ac0a9c004
sarahmurphy86 <sarah.graham86@gmail.com>
2018-07-18 11:29:38 +0100

    Game container created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import GameContainer from './containers/GameContainer';
+
+
+class App extends Component {
+  render() {
+    return (
+      <GameContainer />
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/GameContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class GameContainer extends React.Component {
+  render(){
+    return <h1>Board test  </h1>
+  }
+}
+
+export default GameContainer;
