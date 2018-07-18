52643426ab3116930efdced24d027252ec7bda85
sarahmurphy86 <sarah.graham86@gmail.com>
2018-07-18 15:38:53 +0100

    lab completed

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,12 +2,43 @@ import React from 'react';
 import Square from './Square.js';
 
 class Board extends React.Component {
-  renderSquare(i){
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const newSquares = [ ... this.state.squares];
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = this.state.xIsNext ? 'X' : 0;
+    this.setState({
+      squares: newSquares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render(){
-    const status = "Next player: x";
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? 'X' : '0');
+    }
 
     return(
       <div>
@@ -30,7 +61,26 @@ class Board extends React.Component {
       </div>
     )
   }
+}
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,24 +1,11 @@
 import React from 'react';
 
-class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render(){
-    // return <h1>I am a square</h1>
-    return(
-      <button
-        className ="square"
-        onClick={() => this.setState({value: 'X'})}
-        >
-        {this.state.value}
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
-}
 
 export default Square;
